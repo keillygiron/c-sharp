e3385bce4af100fdec34bb34b2377d45ae2bd3a1
Keilly Giron <gironkeilly@gmail.com>
2021-07-12 00:54:53 -0600

    Agregando Propiedades

--- Ejercicios/3-Propiedades/propiedades/Program.cs
+++ Ejercicios/3-Propiedades/propiedades/Program.cs
@@ -4,29 +4,26 @@ namespace propiedades
 {
     class Program
     {
-      static void Main(string[] args)
+        static void Main(string[] args)
         {
             Alumno a = new Alumno();
             a.PrimerNombre = "Juan";
             a.SegundoNombre = "Perez";
-            
+
             Alumno b = new Alumno();
             b.Id = 2;
             b.PrimerNombre = "Maria";
-            b.SegundoNombre ="Martinez";
-            
-            Alumno c = new Alumno (3);
+            b.SegundoNombre = "Martinez";
+
+            Alumno c = new Alumno(3);
             b.PrimerNombre = "Pedro";
 
             Alumno d = new Alumno("Juan", "Jimenez");
 
-
-
             Console.WriteLine(a.Id);
             Console.WriteLine(b.Id);
             Console.WriteLine(c.Id);
-            Console.WriteLine(b.PrimerNombre + " " + d.SegundoNombre);
-            
+            Console.WriteLine(d.PrimerNombre + " " + d.SegundoNombre);
         }
     }
 }
